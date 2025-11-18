b62f0fe9aebb1104222e9f6a5f2b5b30aed3fad4
jay1535 <habbujayanth@gmail.com>
2025-11-18 23:45:37 +0530

    Added Convex database setup along with Users schema

--- intellidocs/app/provider.jsx
+++ intellidocs/app/provider.jsx
@@ -1,10 +1,14 @@
+"use client";
 import React from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from './_components/AppSidebar';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 function Provider({ children, ...props }) {
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
   return (
+    
    <NextThemesProvider
       attribute="class"
       defaultTheme="light"
@@ -16,10 +20,13 @@ function Provider({ children, ...props }) {
             <AppSidebar/>
             <div className="w-full">
                 <SidebarTrigger className={'rounded-md mx-2 my-2'}/>
+                <ConvexProvider client={convex}>
         {children}
+        </ConvexProvider>
         </div>
         </SidebarProvider>
     </NextThemesProvider>
+    
   )
 }
 
