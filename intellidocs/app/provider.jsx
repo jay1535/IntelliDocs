edd39b4a58be09853c0de8d3cbb32865ee47d87e
jay1535 <habbujayanth@gmail.com>
2025-11-19 19:11:50 +0530

    minor changes to show appSidebar

--- intellidocs/app/provider.jsx
+++ intellidocs/app/provider.jsx
@@ -1,33 +1,50 @@
 "use client";
-import React from 'react'
+import React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from './_components/AppSidebar';
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "./_components/AppSidebar";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 function Provider({ children, ...props }) {
-    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
   return (
-    
-   <NextThemesProvider
+    <NextThemesProvider
       attribute="class"
       defaultTheme="light"
       enableSystem
       disableTransitionOnChange
       {...props}
     >
-        <SidebarProvider >
-            <AppSidebar/>
-            <div className="w-full">
-                <SidebarTrigger className={'rounded-md mx-2 my-2'}/>
-                <ConvexProvider client={convex}>
-        {children}
-        </ConvexProvider>
+      <SidebarProvider>
+        
+        
+        <SignedIn>
+          <AppSidebar />
+        </SignedIn>
+
+        <div className="w-full">
+
+          {/* Sidebar trigger also only when logged in */}
+          <SignedIn>
+            <SidebarTrigger className="rounded-md mx-2 my-2" />
+          </SignedIn>
+
+          {/* When signed out, center everything (optional) */}
+          <SignedOut>
+            <div className="p-4"></div>
+          </SignedOut>
+
+          {/* Convex Provider */}
+          <ConvexProvider client={convex}>
+            {children}
+          </ConvexProvider>
         </div>
-        </SidebarProvider>
+
+      </SidebarProvider>
     </NextThemesProvider>
-    
-  )
+  );
 }
 
-export default Provider
+export default Provider;
